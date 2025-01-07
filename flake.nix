0cb70a2fa4192338e819a3d161b23e58ce1578a5
abrasumente <why-excuse-quail@duck.com>
2025-01-07 18:31:09 +0800

    add nix package

--- flake.nix
+++ flake.nix
@@ -9,10 +9,21 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
   outputs =
-    inputs@{ flake-parts, nixpkgs, ... }:
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      crane,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.devenv.flakeModule
@@ -26,20 +37,28 @@
           system,
           ...
         }:
+
+        let
+          # rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+          };
+          craneLib = crane.lib.${system}.overrideToolchain rustToolchain;
+
+        in
         {
           _module.args.pkgs = import inputs.nixpkgs {
             inherit system;
             overlays = [ (import inputs.rust-overlay) ];
           };
+          packages.default = pkgs.callPackage ./package.nix { inherit craneLib rustToolchain; };
           devenv.shells.default =
             let
               isDarwin = pkgs.lib.strings.hasSuffix "-darwin" system;
-              rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-                extensions = [
-                  "rust-src"
-                  "rust-analyzer"
-                ];
-              };
             in
             {
               packages =
